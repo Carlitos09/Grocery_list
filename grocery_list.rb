f21431bd492c876703007b1d5d81de9d98fd6b85
Carlitos09 <tighty63@hotmail.com>
2014-04-23 17:58:29 +0000

    Created a grocery list

---
+++ grocery_list.rb
@@ -0,0 +1,18 @@
+grocery_list = Hash.new
+puts "What do you want to add to your grocery list?"
+item = gets.chomp
+while item != "exit"
+  if   grocery_list.has_key?(item)  
+     grocery_list[item] +=1
+  else 
+     grocery_list[item] =1
+  end
+  puts "What do you want to add to your grocery list?"
+  item = gets.chomp
+end 
+  
+  
+  
+grocery_list.each do |item,number|
+  puts "#{item} ≈ #{number}"
+end 
